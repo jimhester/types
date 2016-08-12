4eda371ef2741dd29becacd7cc33ee98c9ba529e
Jim Hester <james.f.hester@gmail.com>
2016-08-12 08:12:42 -0400

    Remove `?` from the call

--- R/package.R
+++ R/package.R
@@ -1,6 +1,9 @@
 `?` <- function(e1, e2) {
   if (missing(e2)) {
-    stop("argument missing, with no default")
+    stop(
+      structure(class = "condition",
+        list(call = sys.call(-1),
+             message = "argument is missing, with no default")))
   } else {
     e1
   }
