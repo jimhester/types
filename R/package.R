576c6f3e6d43b5704ad2222c7890fe5a6681b0bf
Jim Hester <james.f.hester@gmail.com>
2016-08-14 16:23:04 -0400

    remove_type function
    Also handle top level `?` calls

--- R/package.R
+++ R/package.R
@@ -1,4 +1,8 @@
 `?` <- function(e1, e2) {
+  if (interactive() && sys.nframe() <= 1L) {
+    call <- get("?", pos = 3L)
+    return(eval(as.call(list(call, substitute(e1), substitute(e2)))))
+  }
   if (missing(e2)) {
     stop(
       structure(class = "condition",
@@ -7,4 +11,43 @@
   } else {
     e1
   }
+}
+
+remove_types <- function (x) {
+    recurse <- function(y) {
+        lapply(y, remove_types)
+    }
+    if (is.atomic(x) || is.name(x)) {
+      x
+    }
+    else if (is.call(x)) {
+      if (identical(x[[1]], as.symbol("?"))) {
+        if (length(x) == 3) {
+          as.call(recurse(x[[2]]))
+        } else {
+          # ?(x) call, This should only occur for arguments with no default
+          quote(expr = )
+        }
+      } else {
+        as.call(recurse(x))
+      }
+    }
+    else if (is.function(x)) {
+        formals(x) <- Recall(formals(x))
+        body(x) <- Recall(body(x))
+        x
+    }
+    else if (is.pairlist(x)) {
+        as.pairlist(recurse(x))
+    }
+    else if (is.expression(x)) {
+        as.expression(recurse(x))
+    }
+    else if (is.list(x)) {
+        recurse(x)
+    }
+    else {
+        stop("Unknown language class: ", paste(class(x), collapse = "/"),
+            call. = FALSE)
+    }
 }
